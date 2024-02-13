89baf9dc7ae98297a0bb1c763d932867be798e07
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-13 14:06:31 +0000

    know the working mechanism of var const and let

---
+++ 01_bascics/01_variables.js
@@ -0,0 +1,14 @@
+let userName = "Manish"
+var userEmail = "manish@gmail.com"
+const userId = 1234
+userAddress = "Itahari"
+
+var phonenumber = ""
+userName = "Mukti"
+userEmail = "mukti@gmail.com"
+// userId = 1254  not allowed
+userAddress = "Dharan"
+console.log(userName)
+// remember there is two types of variables block label and global
+console.table([userName, userEmail, userId, userAddress, phonenumber])
+// prints the result in tabular forms
