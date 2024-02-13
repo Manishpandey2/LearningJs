19ab50500bb675f610bc5b1e3ef847e0aac7e056
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-13 14:31:05 +0000

    datatypes

---
+++ 01_bascics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(5/4) we are using nodejs, not browser
+
+console.log(5/4)
+
+console.log("manish")
+
+let name = "manish"
+let age = 18
+let isLoggedIn = false
+
+// int
+// bigint
+// char
+// string
+// array
+// object 
+// typeof
+// boolean
+/* null this is datatype and also the standalone value if you check 
+ this by using typeof datatypes you see the result is object datatype
+ so you can consider it as object also
+*/
+// undefined value not assigned 
+// symbol
