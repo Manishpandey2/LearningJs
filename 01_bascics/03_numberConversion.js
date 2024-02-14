de79fb6360ca1efc13c759cc3cd046fca5a484a1
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-14 01:25:54 +0000

    conversion of data types

---
+++ 01_bascics/03_numberConversion.js
@@ -0,0 +1,39 @@
+let employeeId = 1234
+
+console.log(typeof employeeId)
+
+let employeeNumber = String(employeeId)
+
+console.log(typeof employeeNumber)
+console.log(employeeNumber)
+
+let employeName = "Manish"
+console.log(typeof employeName)
+
+let employeId = Number(employeName)
+console.log(typeof employeId)
+console.log(employeId)
+// string into number = nan
+
+let sregisternumber = "123"
+console.log(typeof sregisternumber)
+
+let sregistername = Number(sregisternumber)
+console.log(typeof sregistername)
+console.log(sregistername)
+// // "123as" = nan
+// "123" = 123
+let number = 0
+let boolean = Boolean(number)
+console.log(boolean)
+console.log(typeof boolean)
+let boolString = String(boolean)
+console.log(boolString)
+console.log(typeof boolString)
+
+// in boolean true = in string true , false false
+// let boolNumber = Number(boolean)
+// console.log(typeof boolNumber)
+// console.log(boolNumber)
+// true =1 , false = 0
+
