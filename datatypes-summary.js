1aaa6e9758e0ed5761218bfc8ca22772fd72679c
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-22 01:11:15 +0000

    basic datatypes with example

---
+++ datatypes-summary.js
@@ -0,0 +1,12 @@
+// data type can be catogorized as how to store that data and how they can be accessed
+// for ex. callby value and call by reference 
+
+// Primitive
+
+// 7types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
