9e8a1dd16702c5575cbafcc2a3edb6c12eca8846
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-24 10:45:17 +0000

    math and number

---
+++ 01_bascics/05_nums_and_math.js
@@ -0,0 +1,32 @@
+// const number = 400;
+// console.log(number);
+
+// const score = new Number(500);
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(1));
+
+// console.log(number.toFixed(3));
+
+// const anotherNumber = 25.457956458;
+// console.log(anotherNumber.toPrecision(4));
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-NP'));
+
+/*---------------------------Math-------------------------*/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+
+// console.log(Math.floor(4.6));
+// console.log(Math.ceil(4.6));
+
+// console.log(Math.min(4,5,6,54,5));
+// console.log(Math.max(4,5,6,54,5));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
