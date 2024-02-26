4d33cff866949141c07cfad9dd23969fcb01f6e1
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-26 11:10:07 +0000

    use of array and object in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,6 +30,34 @@ function loginUserMessege(username = "You"){
     }
     return `${username} just logged in`;
 }
-console.log(loginUserMessege());
+// console.log(loginUserMessege());
 
 // ----------------very basic of function finished
+
+function calculateCartPrice(num2, num3, ...num1){
+    return num1;
+}
+
+// console.log(calculateCartPrice(100, 200, 300));
+
+const user = {
+    name: "manish",
+    price: 487
+}
+function handleObject(anyobject) {
+    console.log(`${anyobject.name} bought the product of ${anyobject.price}`);
+}
+// handleObject(user);
+// handleObject({
+//     name: "anupam",
+//     price: 788
+// });
+
+const arr = [100, 200, 300 ,400];
+function returnThirdElement(number) {
+    console.log(`The second element of the array is ${number[1]}`);
+}
+// returnThirdElement(arr);
+returnThirdElement(
+    [100, 55465, 6564, 665]
+)
