199752cd502a0064f8eaadc84e420745def8bd83
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-26 15:41:32 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,7 +7,44 @@ let a = 565;
 }
 
 
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
  
+
+// another part
+// nested scope
+
+function one(){
+    const username = "manish";
+    function two(){
+        const website = "github";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+// one();
+
+if(true){
+    const username = "manish";
+    if(username === "manish") {
+        const website = "github";
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// dlfja;lsdf;lajds;fljas;ldkf lau aayo aayo haha dimag kharab lafjldjf;ladsjf;ljasd;lfkjask;ld
+
+// function addone(num){
+//     return num + 1;
+// }
+// console.log(addone(5));
+
+const addTwo = function(num){
+    return num + 1;
+}
+console.log(addTwo(2));
