026954124f81c0f4329381f79c28b47d6102f2a0
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-27 13:54:11 +0000

    if else and elseif

---
+++ 04_controlflow/01_ifelse.js
@@ -0,0 +1,32 @@
+// if 
+
+const isUserLoggedIn = true;
+const isUserTeacher = true;
+const loggedInfromGoogle = true;
+const loggedInfromEmail = false;
+if(loggedInfromEmail || loggedInfromGoogle){
+//     console.log("logged in successfully");
+}
+if (isUserLoggedIn && isUserTeacher) {
+//     console.log("Dear teachers, there will be a mandatory faculty meeting tomorrow to discuss important matters, your punctual attendance is required.");
+}
+const temperature = 41;
+if(temperature === 42){
+//     console.log("School closed, due to the unexpected rise in temperature");
+}else if(temperature <= 41){
+//     console.log("School is open");
+}else{
+//     console.log("enjoy the Holiday");
+}
+
+// <, >, <=, >=, ==,, !=, ===, !==
+
+const score = 10;
+if(score>9){
+    const messi = 50;
+    // console.log(`${messi} is scored by messi`);
+}
+// console.log(`${messi} is scored by messi`);
+
+const balance = 10000;
+// if(balance>8000) console.log("You have enough balance for one day college tour");
