4bcd29f32c6b05518f4d9f693c15c694ca94836f
Manish Pandey <156058874+Manishpandey2@users.noreply.github.com>
2024-02-28 02:10:33 +0000

    while and do while loop

---
+++ 05_iteration/02_while.js
@@ -0,0 +1,21 @@
+// while
+// let i=1;
+// while (i<=10) {
+//     // console.log(`this is ${i}th iteration`);
+//     i = i + 2;
+// }
+
+let myArray = ["mira","rachana","nisha","manish","sabita"];
+let i = 0;
+while (i<myArray.length) {
+    const arr = myArray[i]
+    // console.log(arr);
+    i++;
+}
+
+// do while loop
+let j = 0;
+do {
+    console.log(j);
+    j = j + 3;
+} while (j<10);
